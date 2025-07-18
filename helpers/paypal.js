6acba287adde62810af4c34772538223b67abc67
shivamroy0206 <shivamroy0206@gmail.com>
2025-07-18 12:22:34 +0530

    Update paypal.js

--- helpers/paypal.js
+++ helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "AS20Cpk9Jjw4CJoa7fq3NHhd6ulTOmsZtuS8VUeqIADa01qP3cQncYpc7xZ-Akkr_0-j0jDxjiAoTcIr",
-  client_secret: "EImFZSFbl7aVo9Y_AF6pvtb8iEPlUTbVVEbQ7ct1AH1GvTM-RW2ORsBfiZWhezWLCJTo-oaM-6BAAUXe",
+  client_id: "",
+  client_secret: "",
 });
 
 module.exports = paypal;
