a1b19fcc627a34ff6e3346e89c6c2e62a9b27f11
shivamroy0206 <shivamroy0206@gmail.com>
2025-07-21 01:00:20 +0530

    Update cloudinary.js

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,11 +1,16 @@
 const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api_Key,
+  api_secret: process.env.Cloud_Api_Secret,
 });
+
+const port = process.env.PORT;
 
 const storage = new multer.memoryStorage();
 
